813adba283d9ae1f0ebbc3203cdce81b6c006e71
Henrique Cesar <henrique.soares1@estudante.ufla.br>
2024-10-25 18:39:03 -0300

    Modificaçao Classe C

--- ClassC.cpp
+++ ClassC.cpp
@@ -10,6 +10,7 @@ public:
     ~C();
     void MC1();
     void MC2();
+    void MC3();
 };
 
 C::C(std::string C1, int C2){
@@ -17,10 +18,14 @@ C::C(std::string C1, int C2){
     this->C2 = C2;
 }
 
-void MC1(){
+void C::MC1(){
     std::cout << "MC1\n";
 }
 
-void MC2(){
+void C::MC2(){
     std::cout << "MC2\n";
+}
+
+void C::MC3(){
+    std::cout << "MC3\n";
 }
