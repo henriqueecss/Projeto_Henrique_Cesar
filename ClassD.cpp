8442f30cb6e65d40a66eeeee90e5b31327e58c4e
Henrique Cesar <henrique.soares1@estudante.ufla.br>
2024-10-25 18:37:42 -0300

    Criaçao da classe D

---
+++ ClassD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class D {
+    private:
+        float D1;
+        int D2;
+    public:
+        D(float D1, int D2);
+        ~D();
+        void MD1();
+        void MD2();
+};
+
+D::D(float D1, int D2){
+    this->D1 = D1;
+    this->D2 = D2;
+}
+
+void D::MD1(){
+    std::cout << "MD1\n";
+}
+
+void D::MD2(){
+    std::cout << "MD2\n";
+}
